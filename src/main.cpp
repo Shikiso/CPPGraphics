cc3ef5cffdc6f3a33d8bacde6e595534740f3f16
Shikiso <lewisemerson00@gmail.com>
2025-04-04 20:49:54 +0800

    Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,26 @@ int main(int, char**){
     return -1;
   }
 
-  glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-  
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+ 
+  int colour = 0;
   while (!glfwWindowShouldClose(window)){
     glfwPollEvents();
 
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3ub(colour, colour,0);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5,-0.5);
+    glVertex2f(0, 0.5);
+    glEnd();
+  
+    if (colour >= 255){
+      colour--;
+    }else{
+      colour++;
+    }
 
     glfwSwapBuffers(window);
   }
